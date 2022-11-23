7b4b408e46fa11c1c8c1b55745394029b1785dca
Syed Hassam Kazmi <59208947+Hassamkazmi@users.noreply.github.com>
2022-11-24 01:04:03 +0500

    aa

--- src/Components/SideBarAbout.jsx
+++ src/Components/SideBarAbout.jsx
@@ -3,6 +3,7 @@ import img1 from '../assets/image 5 (1).png'
 import img2 from '../assets/image 7 (1).png'
 import img3 from '../assets/image 8 (1).png'
 import img4 from '../assets/image 7 (1).png'
+import { Link } from 'react-router-dom'
 
 const SideBar = () => {
   return (
@@ -11,10 +12,12 @@ const SideBar = () => {
             <div className='menu'>
                 <div className='item'>
                     <span className='whitebg'>
-                    <span className='text'>About</span>
-                    <span className='navimg'>
+                   <Link to='/' >
+                    <span className='navimg newabout'>
+                    <span className='text1'>Home</span>
                         <img src={img1} alt="img" />
                     </span>
+                    </Link>
                     </span>
                 </div>
                 <div className='item'>
