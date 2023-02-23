933683f1b01daf5281e3cb97bf61d938e05d435c
hammadaslam10m <muhammadfaizraza48@gmail.com>
2023-02-23 21:13:43 +0500

    few changes

--- src/Components/AboutSection.jsx
+++ src/Components/AboutSection.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 import Fade from "react-reveal/Fade";
+import ReadMoreReact from "read-more-react";
+import data from "../Data";
+
 const AboutSection = () => {
   return (
     <React.Fragment>
       <Fade bottom>
-        <div className="section">
-          <p>
-            Salaam Family Takaful Limited is a revolutionary force in the world
-            of takaful, leading the way as Pakistan's premier digital takaful
-            company. Utilizing state-of-the-art technology, SFTL offers its
-            customers a seamless and easy-to-use online experience. With a
-            mission to bring takaful to the masses in Pakistan, SFTL offers an
-            array of diverse takaful products tailored to meet the unique needs
-            of each individual customer. Dedicated to providing unparalleled
-            customer service and delivering products that offer exceptional
-            value, Salaam Family Takaful Limited is truly the premier choice for
-            all your takaful needs.
-          </p>
+        <div className="section features">
+          {data.About.map((item, index) => (
+            <>
+              <h3>{item.title}</h3>
+              <ReadMoreReact
+                text={item.description}
+                min={100}
+                ideal={120}
+                max={300}
+                readMoreText="read more"
+              />
+            </>
+          ))}
         </div>
       </Fade>
     </React.Fragment>
