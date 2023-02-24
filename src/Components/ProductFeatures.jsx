85d2eca9da58f8c18313c7d2f785c067488f6c56
hammadaslam10m <muhammadfaizraza48@gmail.com>
2023-02-24 14:41:34 +0500

    done

--- src/Components/ProductFeatures.jsx
+++ src/Components/ProductFeatures.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 import { Fade } from "react-reveal";
+import ReadMoreReact from "read-more-react";
+import data from "../Data";
 
 const ProductFeatures = () => {
   return (
     <Fade bottom>
       <div className="section features">
-        <h3>Unique Product Features</h3>
-        <ul>
-          <li>Supercharge your investments with 100% allocation </li>
-          <li>Bonus bonanza: 15% in Year 5, 20% in Year 10 </li>
-          <li> Flexible, stress-free contribution payments </li>
-          <li>Limitless access to funds, no withdrawal fees </li>
-          <li> Zero bid-offer charges, No Wakala </li>
-        </ul>
+        {data.Product.map((item, index) => (
+          <>
+            <h3>{item.title}</h3>
+            <ReadMoreReact
+              text={item.description}
+              min={100}
+              ideal={120}
+              max={300}
+              readMoreText="read more"
+            />
+          </>
+        ))}
       </div>
     </Fade>
   );
--- src/Components/ProductRiders.jsx
+++ src/Components/ProductRiders.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 import { Fade } from "react-reveal";
+import ReadMoreReact from "read-more-react";
+import data from "../Data";
 
 const ProductRiders = () => {
   return (
     <Fade bottom>
       <div className="section features">
-        <h3>Supplementary Riders</h3>
-        <ul>
-          <li>Supercharge your investments with 100% allocation </li>
-          <li>Bonus bonanza: 15% in Year 5, 20% in Year 10 </li>
-          <li> Flexible, stress-free contribution payments </li>
-          <li>Limitless access to funds, no withdrawal fees </li>
-          <li> Zero bid-offer charges, No Wakala </li>
-        </ul>
+        {data.Riders.map((item, index) => (
+          <>
+            <h3>{item.subtitle}</h3>
+
+            <ReadMoreReact
+              text={item.description}
+              min={100}
+              ideal={120}
+              max={300}
+              readMoreText="read more"
+            />
+          </>
+        ))}
       </div>
     </Fade>
   );
